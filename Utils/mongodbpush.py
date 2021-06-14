05d6c5ef7e788d324e2fb6d26033d03b15ab65f1
GCO <gco76600@gmail.com>
2021-06-14 21:55:38 +0200

    update scraping

--- Utils/mongodbpush.py
+++ Utils/mongodbpush.py
@@ -2,12 +2,15 @@ import pymongo
 import dns
 
 
-def push_pandas_mongodb(df, conn=None):
+def push_pandas_mongodb(df, table, conn=None):
     if conn == None:
         conn = pymongo.MongoClient(
             'mongodb+srv://HorseUser:4m8J5Xg53t2gBMW@cluster0.dsgpz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     db = conn.client['crypto_database']
-    collection = db['crypto_spot_database_1h']
+    collection = db[table]
     df_dict = df.to_dict("records")
-    for row in df_dict:
-        collection.replace_one({'Open_time': row.get('Open_time'),'Spot':row.get('Spot')}, row, upsert=True)
+    try:
+        for row in df_dict:
+            collection.replace_one({'Open_time': row.get('Open_time'), 'Spot': row.get('Spot')}, row, upsert=True)
+    except Exception as e:
+        print(e)
